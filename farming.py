f12a44bcd378b7cec128acb180e53a23091196dd
Glooze <ttglooze@gmail.com>
2026-01-14 23:42:54 +0100

    pumpkin fix
    pumpkin fix by iterating over a copy of the list to prevent index shifting due to removing elements while iterating over list

--- farming.py
+++ farming.py
@@ -123,7 +123,7 @@ def pumpkin():
 
     # 3. Only check the replaced pumpkins
     while len(replaced_pumpkins) > 0:
-        for pos in replaced_pumpkins:
+        for pos in replaced_pumpkins[:]:   # ← iterate o=ver a copy of the list to prevent index shifting
             util.goto(pos[0], pos[1])
             if get_entity_type() == Entities.Dead_Pumpkin:
                 plant(Entities.Pumpkin)
